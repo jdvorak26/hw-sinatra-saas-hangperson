6264acbfac85e7ab7677960828fdba45ece50e81
Josh <jdvorak@citadel.edu>
2016-10-05 01:58:13 +0000

    /guess method still needs work

--- app.rb
+++ app.rb
@@ -40,17 +40,6 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    #INVOKE GUESS METHOD FROM GAME LOGIC
-    
-    if letter
-      flash[:message] = "Invalid guess."
-    end
-    
-    if letter
-      flash[:message] = "You have already guessed that letter."
-    end
-    
-    redirect '/show'
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -60,19 +49,32 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      flash[:message] = 'You Win!'
+      erb :win
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose
+      flash[:message] = 'Sorry, you lose!'
+    else
+      redirect '/show'
+    end
   end
-  
 end
