5d5d6475572a96706e6627a6708c74fdf926eaad
Josh <jdvorak@citadel.edu>
2016-10-05 02:08:40 +0000

    1 still

--- app.rb
+++ app.rb
@@ -57,14 +57,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    status = @game.check_win_or_lose
-    case status
-    when :win
+    if @game.check_win_or_lose == :win
       redirect '/win'
-    when :lose
+    elsif @game.check_win_or_lose == :lose
       redirect '/lose'
     else
-       erb :show 
+      erb :show
     end
   end
   
